a73984f9a478fed99331af6496f0859c7194c061
Adnan <adnanm281999@gmail.com>
2021-11-28 20:26:43 +0530

    Added Hom page

---
+++ REACT-UI/clinic-management-system/src/Homepage.js
@@ -0,0 +1,22 @@
+function HomePage(){
+    return(
+      <>
+      <div>
+        <h1>Home Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Morbi sit amet gravida sem, a faucibus tortor. 
+          Vivamus eleifend vulputate orci, a iaculis lectus cursus nec. 
+          Nulla vulputate eros in leo placerat, ullamcorper vestibulum dui malesuada. 
+          ec tempor lacus nec lorem molestie, non tincidunt metus lacinia. Etiam eget nibh elit. 
+          Curabitur sed tristique quam. Nulla a hendrerit justo, at bibendum sapien. 
+          Integer ac nunc sed ligula porttitor pellentesque vitae sed erat. Vestibulum quis neque nisl. 
+          Donec pulvinar sem in venenatis congue. Aliquam at nisi eu augue finibus feugiat. 
+          Phasellus volutpat porttitor felis, et dictum dolor facilisis nec. 
+          Fusce vulputate ut dui nec egestas. Nullam eu neque quis nibh luctus fringilla. 
+          Curabitur egestas tortor quam, sit amet laoreet lectus mollis quis.</p>
+      </div>
+      </>
+    );
+  };
+  
+  export default HomePage;
