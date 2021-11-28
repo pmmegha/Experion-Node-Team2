06c718df683c460c66bc3496d34364e53fd2495a
Adnan <adnanm281999@gmail.com>
2021-11-28 20:28:19 +0530

    Added NoMatch page

---
+++ REACT-UI/clinic-management-system/src/NoMatch.js
@@ -0,0 +1,12 @@
+function NoMatch(){
+    return(
+      <>
+      <div>
+        <h1>Oops...</h1>
+        <h2>You seem to have reached a non existing page.</h2>
+      </div>
+      </>
+    );
+  };
+  
+  export default NoMatch;
