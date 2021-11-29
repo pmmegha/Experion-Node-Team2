7e81f164072812173d7715c0f470a86c830a1790
Adnan <adnanm281999@gmail.com>
2021-11-29 10:22:01 +0530

    Editted about

--- REACT-UI/clinic-management-system/src/About.js
+++ REACT-UI/clinic-management-system/src/About.js
@@ -11,9 +11,7 @@ function About(){
           Curabitur sed tristique quam. Nulla a hendrerit justo, at bibendum sapien. 
           Integer ac nunc sed ligula porttitor pellentesque vitae sed erat. Vestibulum quis neque nisl. 
           Donec pulvinar sem in venenatis congue. Aliquam at nisi eu augue finibus feugiat. 
-          Phasellus volutpat porttitor felis, et dictum dolor facilisis nec. 
-          Fusce vulputate ut dui nec egestas. Nullam eu neque quis nibh luctus fringilla. 
-          Curabitur egestas tortor quam, sit amet laoreet lectus mollis quis.</p>
+          Phasellus volutpat porttitor felis, et dictum dolor facilisis nec.</p>
       </div>
       </>
     );
